0001646ef6aa814ded13330a5e62953e61853dc8
ksbhokre768-eng <ksbhokre768@gmail.com>
2026-09-17 13:09:55 +0530

    new edit

---
+++ tut10.cpp
@@ -0,0 +1,42 @@
+#include<iostream>
+using namespace std;
+int main(){
+// loops in c++
+// 1.for  loop
+// 2. while loop 
+// 3.do-while loop 
+
+// for loop
+
+// for(initialization; condition;  updation){
+// loop body (c++ code)
+// }
+
+
+// cout<<"frist 45 whole numbers are"<<endl;
+// for(int i=0 ;i<45 ; i++){
+// // cout<<"frist 45 whole numbers are"<<endl;
+// cout<<i<<endl;
+// cout<<"frist 45 whole numbers are"<<endl;
+
+
+// infinite loop
+ 
+// for(int i=0 ;34<45 ; i++){
+// // cout<<"frist 45 whole numbers are"<<endl;
+// cout<<i<<endl; 
+
+// while loop
+
+//  while(condition):
+// { statement c++ code;
+// }
+
+// printing 1 to 50
+int i=1;
+while(i<=50){
+    cout<<i<<endl;
+    i++;
+}
+  return 0;
+}
